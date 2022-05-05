3779a02405716761769e043c72fb7d06e222a719
sbush92 <sjbush92@gmail.com>
2022-05-05 10:45:05 -0600

    post

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const EthSwap = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
 };
-
-
